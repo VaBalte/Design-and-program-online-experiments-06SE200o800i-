6c6435c37d6541df1057c762e5fd8e5720554d97
Valerie Baltensperger <valerie.baltensperger@uzh.ch>
2026-03-20 08:58:16 +0100

    exercise 5.1

---
+++ VB_Session5/STARTER/session5.js
@@ -0,0 +1,55 @@
+// 5.1.1
+/* let ageGroup;
+let age = 25;
+let age = 10
+if (age < 18) {
+    ageGroup = "Child";
+}
+    else
+        {ageGroup="Adult";    
+}
+
+console.log("ageGroup", ageGroup)
+*/
+
+// 5.1.2
+/* let age = 18
+let ageGroup = age < 18 ? "Child" : "Adult";
+*/
+
+// 5.1.4
+/* for (let i = 0; i < 5; i++) {
+    // console.log(Number: ${i}`);
+    console.log("Number:", i);
+}
+*/
+
+// 5.1.5
+/*const responseTimes = [350, 420, 510];
+for (const time of responseTimes) {
+    console.log(`Response Time: ${time} ms`);
+}` 
+
+const trial = { id: 1, responseTime: 350, correct: true };
+for (const key in trial) {
+console.log(`${key}: ${trial[key]}`);
+}
+*/
+
+// 5.1.6
+/* let i = 0;
+while (i < 5) {
+    console.log (`Number: ${i}`);
+    i++;
+}
+*/
+// 5.1.7
+let i = 0;
+do {
+    console.log(`Number: ${i}`);
+    console.log(i);
+    i++;
+    console.log(i);
+   
+} while (i < 5)
+
